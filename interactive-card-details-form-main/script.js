4cec40f19f828ad27fa2e024548850199edc7629
Adarsh Singh <adarshsingh452002@gmail.com>
2022-08-09 17:35:37 +0530

    frontend-projects

---
+++ interactive-card-details-form-main/script.js
@@ -0,0 +1,7 @@
+const cardholder= document.getElementById("cardholder-name");
+const cardNumber= document.getElementById("card-number");
+const expiry= Array.from(document.querySelectorAll(".expiry"));
+const cvc= document.getElementById("cvc");
+const submit= document.getElementById("submit");
+const nameOnCard= document.querySelector(".cardholder-display");
+
